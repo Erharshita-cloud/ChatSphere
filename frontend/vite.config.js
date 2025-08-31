2b27764d9be45e57e33bb009d731bb6df0fcdab7
Harshita Goel <harshitagoel1503@gmail.com>
2025-08-31 19:35:21 +0530

    k8s Deployed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 change from '/lander/' to '/'
+  server: {
+   host: '0.0.0.0',   // important: listens on 0.0.0.0
+    port: 80      // container port matches service targetPort
+  }
 })
